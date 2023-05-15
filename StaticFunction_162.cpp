2b29cc38ce4010afada8896c57a7a8da0125b936
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2023-05-15 15:34:20 +0700

    menambahkam member

--- StaticFunction_162.cpp
+++ StaticFunction_162.cpp
@@ -3,5 +3,12 @@
 using namespace std; 
 
 class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
 
+	void setID();
+	void printALL();
+	mahasiswa(string pnama) :nama(pnama) { setID() }
 };
